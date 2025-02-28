4c181ecda6229badab9a34ba95a908c6c0b1086c
username <useremail>
2025-02-28 08:24:36 -0800

    all

--- Rectangle.py
+++ Rectangle.py
@@ -3,27 +3,28 @@
 #Rectangle
 
 class Rectangle:
+    
     def __init__(self, height, width):
         self.height = height
         self.width = width
         
-def calculate_perimeter(self):
-    return 2 * (self.height + self width)
+    def calculate_perimeter(self):
+        return 2 * (self.height + self .width)
 
-def calculate_area(self):
-    return self.height * self.width
+    def calculate_area(self):
+        return self.height * self.width
 
-def print_rectangle(self):
-    print(f"Height: {self.height}")
-    print(f"Width: {self.width}")
-    print(f"Perimeter: {self.calculate_perimeter()}")
-    print(f"Area: {self.calculate_area()}")
+    def print_rectangle(self):
+        print(f"Height: {self.height}")
+        print(f"Width: {self.width}")
+        print(f"Perimeter: {self.calculate_perimeter()}")
+        print(f"Area: {self.calculate_area()}")
     
-    for i in range(self.height):
-        if i == 0 or i == self.height - 1:
-            print('*' * self.width)
-        else:
-            print('*' + ' ' * (self.width - 2) + '*')
+        for i in range(self.height):
+            if i == 0 or i == self.height - 1:
+                 print('*' * self.width)
+            else:
+                print('*' + ' ' * (self.width - 2) + '*')
             
 def main():
     while True:
@@ -33,6 +34,7 @@ def main():
         width = int(input("Enter width: "))
         
         rect = Rectangle(height, width)
+       
         
         rect.print_rectangle()
         
